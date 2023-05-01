ce1d6d0db7548dc4e5a4a7a3f8a992ed8b1251f7
nonamecat19 <113713781+nonamecat19@users.noreply.github.com>
2023-05-02 00:31:13 +0300

    added gitignore to server

--- client/main.tsx
+++ client/main.tsx
@@ -1,10 +1,13 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import {RouterProvider} from "react-router-dom"
-import router from './src/router'
+import Router from './src/components/Router'
+import './src/shared/styles/global.style.css'
+import AntDesignConfigProvider from "./src/components/AntDesignConfigProvider";
 
 createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <AntDesignConfigProvider>
+            <Router/>
+        </AntDesignConfigProvider>
     </StrictMode>
 )
