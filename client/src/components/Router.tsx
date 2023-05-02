5ff6301ab2849647eeaa6c42cd2fbf03ee573b17
nonamecat19 <113713781+nonamecat19@users.noreply.github.com>
2023-05-02 10:20:45 +0300

    backend test

--- client/src/components/Router.tsx
+++ client/src/components/Router.tsx
@@ -9,11 +9,10 @@ const Router: FC = () => {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<MainLayout/>}>
-                    <Route path='/1' element={<div>1</div>}/>
                     <Route path={PERSON} element={<div>Person</div>}/>
                     <Route path={ME} element={<div>Me</div>}/>
                     <Route path={ROOMS} element={<div>Кімната</div>}/>
-                    <Route path='/5' element={<div>5</div>}/>
+                    <Route path='/*' element={<div>NO ROUTE FOUND</div>}/>
                 </Route>
                 <Route path={LOGIN} element={<Login/>}/>
             </Routes>
