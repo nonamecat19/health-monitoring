ab5e118bfc128f7795f7dd5faa9f8d34d5d23316
Shumsky Alexander <vt211_shov@student.ztu.edu.ua>
2023-05-31 01:25:01 +0300

    server env

--- server/server.js
+++ server/server.js
@@ -5,8 +5,6 @@ const mainRouter = require('./src/routes/mainRouter')
 const app = express()
 const PORT = 3000
 
-const PAGE_RECORDS = 5
-
 const corsOption = {
     origin: ['http://localhost:5173'],
 }
