1c697c0bb085bd5044c48b0f9c8ea6fc559ba7eb
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-02-07 23:47:28 +0200

    feat: user service crud implementation

--- server/src/modules/user/services/user.service.ts
+++ server/src/modules/user/services/user.service.ts
@@ -1,16 +1,33 @@
 import {Injectable} from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {User} from '../entities/user.entity';
-import {Repository} from 'typeorm';
+import {DeleteResult, Repository} from 'typeorm';
+import {CrudOperations} from '@shared/interfaces/crud-operations.interface';
 
 @Injectable()
-export class UserService {
+export class UserService implements CrudOperations<User> {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>
   ) {}
 
-  public async getAllUsers() {
+  public async create(fields: Omit<User, 'id'>): Promise<User> {
+    throw new Error('Method not implemented.');
+  }
+
+  public async getAll(): Promise<User[]> {
     return this.userRepository.find();
+  }
+
+  public async getOne(id: number): Promise<User> {
+    return this.userRepository.findOneBy({id});
+  }
+
+  public async edit(id: number, fields: Partial<User>): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+  public async delete(id: number): Promise<DeleteResult> {
+    return this.userRepository.delete({id});
   }
 }
