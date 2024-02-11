2985e5b8c69c0384e7aa4680b76e904b601c47fd
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-02-12 00:02:56 +0200

    test: mapperService

--- server/src/shared/services/mapper.service.spec.ts
+++ server/src/shared/services/mapper.service.spec.ts
@@ -15,4 +15,32 @@ describe('MapperServiceService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('map1', () => {
+    const object = {id: 2};
+    const config = {id: 'userId'};
+    const result = service.map(object, config);
+    expect(result).toStrictEqual({userId: 2});
+  });
+
+  it('mapArray1', () => {
+    const array = [{id: 1}, {id: 2}];
+    const config = {id: 'userId'};
+    const result = service.mapArray(array, config);
+    expect(result).toStrictEqual([{userId: 1}, {userId: 2}]);
+  });
+
+  it('mapNested1', () => {
+    const object = {user: {id: 2}};
+    const config = {'user.id': 'userId'};
+    const result = service.map(object, config);
+    expect(result).toStrictEqual({user: {userId: 2}});
+  });
+
+  it('mapNested2', () => {
+    const object = {people: {user: {id: 2}}};
+    const config = {'people.user.id': 'userId'};
+    const result = service.map(object, config);
+    expect(result).toStrictEqual({people: {user: {userId: 2}}});
+  });
 });
